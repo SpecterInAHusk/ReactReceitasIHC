981993c0d34a50a7c94559ce74e04e25739cbdb3
LorenzoPrograming <lm.pilati@gmail.com>
2023-08-24 13:46:18 -0300

    copiuado dos filmes

---
+++ receitas/src/pages/Home/index.js
@@ -0,0 +1,13 @@
+import { Link } from 'react-router-dom';
+import './home.css';
+
+// URL DA API: /movie/now_playing?api_key=28fc232cc001c31e8a031f419d0a14ca&language=pt-BR
+
+function Home(){
+
+  return(
+   <p>Home</p>
+  )
+}
+
+export default Home;
