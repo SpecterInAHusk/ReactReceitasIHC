c74027705c3572933e5369c08db87f6facf10c15
LorenzoPrograming <lm.pilati@gmail.com>
2023-08-28 21:14:17 -0300

    Fim da Gambiarra

--- receitas/src/pages/Detalhes/index.js
+++ receitas/src/pages/Detalhes/index.js
@@ -1,17 +1,28 @@
 import { useParams } from 'react-router-dom';
 
 function Detalhes(){
-    const { id } = useParams();
-    const receitas = require('../../receitas.json');
+    var { id } = useParams();
+    var receitas = require('../../receitas.json');
+
+    function findById(receitas, id) {
+        for (var i = 0; i < receitas.length; i++) {
+            if (receitas[i].id === id) {
+                return receitas[i];
+            }
+        }
+    }
+
+    const receita = findById(receitas, id);
+
 
     return(
         <div>
-            <h2>{receitas[id-1].titulo}</h2>
+            <h2>{receita.titulo}</h2>
 
             <div>
             <div>Ingredientes:</div>
             <ul>
-            {receitas[id-1].ingredientes.map((ingrediente) =>{
+            {receita.ingredientes.map((ingrediente) =>{
                 return(
                 <li>{ingrediente}</li>
                 )
@@ -23,7 +34,7 @@ function Detalhes(){
 
             <div>
             <div>Modo de Preparo:</div>
-            <p>{receitas[id-1].modoPreparo}</p>  
+            <p>{receita.modoPreparo}</p>  
             </div>
 
             <br/>
